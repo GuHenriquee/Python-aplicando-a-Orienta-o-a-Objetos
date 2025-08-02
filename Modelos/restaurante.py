7e5a3ee41dd2edab807e4710ebf64c071e37af89
GuHenriquee <gguu.henri.com>
2025-08-02 16:35:31 -0300

    Objetos e modularização

---
+++ Modelos/restaurante.py
@@ -0,0 +1,26 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print (f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
+
+    @property
+    def ativo(self):
+        return 'Aberto' if self._ativo else 'Fechado'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+
---
+++ app.py
@@ -0,0 +1,12 @@
+from Modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praca', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
