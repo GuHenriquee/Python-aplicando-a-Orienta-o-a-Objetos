90a30229ef9f3a13ffb0e7379d7bc850b7fe75dc
GuHenriquee <gguu.henri.com>
2025-08-04 18:27:59 -0300

    Herança

---
+++ Modelos/cardapio/prato.py
@@ -0,0 +1,5 @@
+from Modelos.cardapio.itemcardapio import
+
+class Prato:
+   def __init__(self, nome, preco, descricao):
+      pass
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from Modelos.restaurante import Restaurante
+from Modelos.cardapio.bebida import Bebida
+from Modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praca', 'Gourmet')
 restaurante_praca.receber_avaliacao ('Gui', 10)
 restaurante_praca.receber_avaliacao ('Eu', 8)
 
+
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == '__main__':
     main()
